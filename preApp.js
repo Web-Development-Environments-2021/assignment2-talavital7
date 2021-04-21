8d2e5cc3dad09f6b0d6f197e1614eff5055780e8
Naor Aspir <aspir@amazon.com>
2021-04-21 20:11:12 +0300

    almost done sign up submittion

--- preApp.js
+++ preApp.js
@@ -1,23 +1,17 @@
-var poll;
+var validForm;
 saveKUser();
 function saveKUser() 
 {
-    var userName="k";
+    var username="k";
     var password="k";
-    sessionStorage.setItem(userName,password);
+    sessionStorage.setItem(username,password);
 } 
 function PageLoaded()
 {
     ShowSection('welcomeScreen');
-    poll = setInterval(function()
-	{
-		// var current_style = document.getElementById('gameScreen').style.display;
-		// if (current_style == "none") {
-		// 	endGame();
-		// }
-	}, 100);
 }
-//pick between Divs
+
+//choose section to show.
 function ShowSection(id)
 {
     //hide all Divs
@@ -25,8 +19,8 @@ function ShowSection(id)
     section1.style.display="none";
     var section2 = document.getElementById('signUp');
     section2.style.display="none";
-    // var section3 = document.getElementById('login');
-    // section3.style.display="none";
+    var section3 = document.getElementById('login');
+    section3.style.display="none";
     var section4 = document.getElementById('gameScreen');
     section4.style.display="none";
     // var section4 = document.getElementById('setups');
@@ -40,3 +34,89 @@ function ShowSection(id)
     
     $(".error").remove();
 }
+
+//Submition button has been clicked in sign up page
+function Submition(){
+    validForm=true;
+    var username = $('#usernameSignUp').val();
+    var fullname = $('#fullname').val();
+    var email = $('#email').val();
+    var password = $('#passwordSignUp').val();
+
+    $(".error").remove();
+
+    //username validation 
+    if (username.length <= 0) {
+        $('#usernameSignUp').after('<span class="error">Please Enter Username</span>');
+        validForm=false;
+    }
+    else{
+        var regEx = /^[a-zA-Z]+$/;
+        var test = regEx.test(username);
+        if (!test) {
+        $('#username').after('<span class="error">A valid username contains only English charecters</span>');
+        validForm=false;
+        }
+    }
+
+    //pasword validation
+    if (password.length <= 0) {
+        $('#passwordSignUp').after('<span class="error">Please Enter Password</span>');
+        validForm=false;
+    }
+    else if (password.length < 6) {
+        $('#passwordSignUp').after('<span class="error">Password must be at least 6 characters long</span>');
+        validForm=false;
+    }
+    else{
+        var regEx = /[a-z].[0-9]|[0-9].[a-z]/i;
+        var test = regEx.test(password);
+        if (!test) {
+        $('#passwordSignUp').after('<span class="error">Enter password with charecters and digits</span>');
+        validForm=false;
+        }
+    }
+
+    //full name validation
+    if (fullname.length <= 0) {
+        $('#fullname').after('<span class="error">Please Enter full name</span>');
+        validForm=false;
+    }
+    else{
+        var regEx = /^[a-z][a-z\s]*$/;
+        var test = regEx.test(fullname);
+        if (!test) {
+        $('#fullname').after('<span class="error">A valid full name contains only English charecters</span>');
+        validForm=false;
+        }
+    }
+
+    //email validation
+    if (email.length <= 0) {
+        $('#email').after('<span class="error">Please Enter Email</span>');
+        validForm=false;
+    } else {
+        var regEx = /^\S+@\S+\.\S+$/;
+        var test = regEx.test(email);
+        if (!test) {
+        $('#email').after('<span class="error">Enter a valid email</span>');
+        validForm=false;
+        } 
+    }
+   
+    //if all requirments are met, create new user.
+    if(validForm){
+    saveUser(); 
+    alert("registered successfully!");
+    $("#regForm")[0].reset();
+    $(".error").remove();
+    ShowSection('login');
+    }
+}
+function saveUser() 
+{
+    var username=document.getElementById("username");
+    var password=document.getElementById("password");
+    sessionStorage.setItem(username.value,password.value);
+    
+}   
